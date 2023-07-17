a6a091456e548be84cf6a00c67a4edff8eb0841a
EFFECT <139741658+effectmoe@users.noreply.github.com>
2023-07-17 22:21:27 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,8 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '95baea1e510046a09d55b6541a4b12f7',
-  
+  rootNotionPageId: 'Referral-Market-Co-Ltd-95baea1e510046a09d55b6541a4b12f7',
+ 
  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)

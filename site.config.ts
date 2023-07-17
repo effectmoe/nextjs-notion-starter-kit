21cb9123e549f862b95ff22693289c75021f8544
EFFECT <139741658+effectmoe@users.noreply.github.com>
2023-07-17 19:09:24 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7b59cf864f904a819730135672ead892',
+  rootNotionPageId: '0346a31f63ec4b3784507e27e7ff3df5',
   
  
   // if you want to restrict pages to a single notion workspace (optional)
